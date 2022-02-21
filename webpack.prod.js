a6fb5f60a509c3cf724543d47d0f1e6373c3e083
Manuel Martinez Gomez <manu.martinez.gomez@gmail.com>
2022-02-21 12:55:00 +0100

    Skeleton upload

---
+++ webpack.prod.js
@@ -0,0 +1,90 @@
+
+const HtmlWebPack = require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+
+    output: {
+        clean: true,
+        filename: 'main.[fullhash].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+             { 
+                test: /styles\.css$/,
+                use: [MiniCssExtract.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    }, 
+
+    devServer: {
+        open: {
+            app: {
+                name: 'Google Chrome',
+            },
+        },
+        port: 8083,
+    },
+    
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser()
+        ],
+    },
+
+    plugins: [
+        new HtmlWebPack({
+            title: 'Cambio titulo',
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/'},
+             ],
+        }),
+    ],
+}
